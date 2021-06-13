a85ce00854685a63e2001d56cd85c5c86009fda4
Mozart Diniz <mozart.diniz@gmail.com>
2021-06-13 11:52:37 +0100

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Mozart Diniz on 13/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
